0c3592b8d87249d3d228b16896fc7c9d096cff84
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-01-27 18:39:05 -0500

    add answer for 03 task

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let str = "";
+    for (let i = string.length; i >= 0; i--){
+        str += string.charAt(i);
+    }
+    console.log(str.toString())
+    return str.toString()
 };
 
 // Do not edit below this line

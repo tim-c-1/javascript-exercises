95dda557de0206d6138a96f94ff940a007717f4d
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-01-27 21:34:31 -0500

    add solution for 04 assignment. found it hard.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    let startLength = array.length;
+    console.log("start args: ", args, "\nstart array: ", array);
+    for (let i = 0; i <= startLength; i++){
+        // console.log(args[i]);
+        let index = array.indexOf(args[i]);
+        // console.log("index: ", index);
+        if (index > -1){
+        array.splice(index, 1);
+        }
+        console.log("i: ",i, "\nlength: ",array.length);
+    }
+    // let newarr = array.filter(a => a != args)
+    if (array.length > 0){
+        for (let i =0; i <= array.length; i++){
+            let index = array.indexOf(args[i]);
+            if (index > -1){
+                array.splice(index, 1);
+            }
+        }
+    }
+    return array
 };
+    
+
+console.log("end array: ", removeFromArray([1,2,2,4,5,"test"],2,4,1,"test"))
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray;   

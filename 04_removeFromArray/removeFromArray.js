23b685fcc95367b9cca354f5ad2472fc72f56b8e
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-01-29 10:13:18 -0500

    updated to simpler function that will work on any number of multiple of same values.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,30 +1,30 @@
 const removeFromArray = function(array, ...args) {
     
-    let startLength = array.length;
-    console.log("start args: ", args, "\nstart array: ", array);
-    for (let i = 0; i <= startLength; i++){
-        // console.log(args[i]);
-        let index = array.indexOf(args[i]);
-        // console.log("index: ", index);
-        if (index > -1){
-        array.splice(index, 1);
-        }
-        console.log("i: ",i, "\nlength: ",array.length);
-    }
-    // let newarr = array.filter(a => a != args)
-    if (array.length > 0){
-        for (let i =0; i <= array.length; i++){
-            let index = array.indexOf(args[i]);
-            if (index > -1){
-                array.splice(index, 1);
-            }
-        }
-    }
-    return array
+    // let startLength = array.length;
+    // console.log("start args: ", args, "\nstart array: ", array);
+    // for (let i = 0; i <= startLength; i++){
+    //     // console.log(args[i]);
+    //     let index = array.indexOf(args[i]);
+    //     // console.log("index: ", index);
+    //     if (index > -1){
+    //     array.splice(index, 1);
+    //     }
+    //     console.log("i: ",i, "\nlength: ",array.length);
+    // }
+    let newarr = array.filter(a => !args.includes(a));
+    // if (array.length > 0){
+    //     for (let i =0; i <= array.length; i++){
+    //         let index = array.indexOf(args[i]);
+    //         if (index > -1){
+    //             array.splice(index, 1);
+    //         }
+    //     }
+    // }
+    return newarr
 };
     
 
-console.log("end array: ", removeFromArray([1,2,2,4,5,"test"],2,4,1,"test"))
+console.log("end array: ", removeFromArray([1,2,2,4,5,"test"],2,5,4,1,"test"))
 
 // Do not edit below this line
 module.exports = removeFromArray;   

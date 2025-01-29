13b7ec584b75529cd4f7951e6e41c3126df8ed27
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-01-29 12:22:28 -0500

    Add answer for temp conversions.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  celsiusTemp = ((temp - 32) * (5/9));
+  celsiusTemp = +celsiusTemp.toFixed(1);
+  console.log(celsiusTemp);
+  return celsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fahrenheitTemp = ((temp * (9/5)) + 32);
+  fahrenheitTemp = +fahrenheitTemp.toFixed(1);
+  console.log(fahrenheitTemp);
+  return fahrenheitTemp;
 };
+
+convertToCelsius(-32);
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {

a2584d76e05c71278aa7880f6d98b0afdb1fad1d
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-01-29 10:55:43 -0500

    Add answer for leap year assignment

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+        return true;
+    } else {
+        return false;
+    }
 };
+
+//divisible by 4, NOT divisible by 100 unless divisible by 400.
+// console.log(leapYears(2001));
+// console.log(leapYears(2004));
 
 // Do not edit below this line
 module.exports = leapYears;

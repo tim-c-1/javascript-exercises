d8da01f88ef4df5e44c7fa14245450c93a579fc4
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-02-02 20:51:28 -0500

    add answer for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    let cleaned = word
+        .toLowerCase()
+        .split("")
+        .filter((c) => alphanumerical.includes(c))
+        .join('');
+   
+    let reversed = cleaned.split("").reverse().join('');
+
+    console.log(reversed);
+    console.log("word: ", cleaned, "newWord: ", reversed);
+    if(cleaned == reversed){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

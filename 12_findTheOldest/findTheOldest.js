ac33b7ed8080a472ab1d3ab7f7e3e8b25e480875
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-02-04 16:34:35 -0500

    add answer for find the oldest assignment.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const today = new Date();
+    const year = today.getFullYear();
 
+    for (const person in arr){
+        if (arr[person].yearOfDeath === undefined){
+            arr[person].yearOfDeath = year;
+        }
+    }
+
+    let ages = arr.map((person) => person.yearOfDeath - person.yearOfBirth);
+    console.log(ages);
+    let oldest = Math.max(...ages);
+    return arr[ages.indexOf(oldest)];
 };
-
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ];
+// console.log(people[0].name);
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;

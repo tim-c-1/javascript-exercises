f5c4b5567167c5d39da0789f5c1ca05f51578430
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-02-04 15:39:10 -0500

    Add answer for fibonacci assignment

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    n = parseInt(num);
+    let fibs = [0,1];
+    if(n < 0){
+        return "OOPS";
+    } else {
+        for (let i = 2; i<=n; i++){
+            let x = fibs[i-2];
+            let y = fibs[i-1];
+            fibs.push(x + y); 
+            // console.log(fibs);
+        }
+        return fibs[n];
+    }
 };
-
+console.log(fibonacci(100));
 // Do not edit below this line
 module.exports = fibonacci;

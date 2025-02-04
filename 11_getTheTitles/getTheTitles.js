81dc0bccab10b08d01ccdf63b59980abee943986
Tim <153627100+tim-c-1@users.noreply.github.com>
2025-02-04 15:54:20 -0500

    Add answer for getTheTitles assignment

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let titles = [];
+    for (const book in books){
+        titles[book] = books[book].title;
+    }
+    return titles;
+}
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
-};
-
+// console.log(books[0].title);
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
